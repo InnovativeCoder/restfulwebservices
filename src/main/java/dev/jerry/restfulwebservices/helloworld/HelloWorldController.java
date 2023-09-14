fec8c59f34f9e59c7fed3aca6818c5f0a49d431c
InnovativeCoder <aashitsingh2010@gmail.com>
2023-09-14 16:35:48 -0400

    variable path name

--- src/main/java/dev/jerry/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/dev/jerry/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package dev.jerry.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,11 @@ public class HelloWorldController {
     @GetMapping( path="/hello-world-bean")
     public HelloWorldBean HelloWorldBean(){
         return new HelloWorldBean("Hello World");
+    }
+
+    //Variable Path Name
+    @GetMapping(path="/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name){
+        return new HelloWorldBean("Hello World "+name);
     }
 }
