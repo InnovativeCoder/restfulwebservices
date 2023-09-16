cb9de5a1511640204e1036c490869d76ec1c09e8
InnovativeCoder <aashitsingh2010@gmail.com>
2023-09-16 11:22:43 -0400

    Added ther custom error response

---
+++ src/main/java/dev/jerry/restfulwebservices/exception/CustomizedRepsonseExceptionHandler.java
@@ -0,0 +1,21 @@
+package dev.jerry.restfulwebservices.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDate;
+
+@ControllerAdvice
+public class CustomizedRepsonseExceptionHandler extends ResponseEntityExceptionHandler {
+
+    //Making a custom response for error
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request){
+        ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
